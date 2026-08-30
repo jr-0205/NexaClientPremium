c626de83d58f6db60a313d89a923a6f92b729914
Carlos <carlossama359@gmail.com>
2026-08-30 14:37:27 -0600

    Load account approval styles

--- src/NexaLauncher.UI/src/main.tsx
+++ src/NexaLauncher.UI/src/main.tsx
@@ -14,6 +14,7 @@ import "./styles/installed-content-icons.css";
 import "./styles/account.css";
 import "./styles/polish.css";
 import "./styles/wizard-review.css";
+import "./styles/account-gate.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
