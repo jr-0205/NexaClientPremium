3d039ebef36f64829c8f18bcc3f1e9071f854691
Carlos <carlossama359@gmail.com>
2026-08-30 14:35:13 -0600

    Load final UI polish layer

--- src/NexaLauncher.UI/src/main.tsx
+++ src/NexaLauncher.UI/src/main.tsx
@@ -12,6 +12,7 @@ import "./styles/build-manager.css";
 import "./styles/build-family.css";
 import "./styles/installed-content-icons.css";
 import "./styles/account.css";
+import "./styles/polish.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
